dac605597d6e22d768d8ba62f30437a769ae4cdb
iakon <iakonien@gmail.com>
2022-06-28 23:00:18 +0300

    Сделал козырьки лицевой стороны

--- src/figures/TrafficLights.java
+++ src/figures/TrafficLights.java
@@ -10,13 +10,11 @@ public class TrafficLights extends Canvas {
 
         g.drawString(String.valueOf(115+460-34/2-5),50,50);
 
-        //Прямоугольники
+        //Корпус
         g.setColor(new Color(50,50,50));
         //g.fillRect(325,120,150,460);
         g.fillRect(321,132,158,438);//Заполнение ширины
         g.fillRect(330,117,140,468);//Заполнение высоты
-
-        //Углы
         g.fillArc(320,115, 34, 34,90,90);//upleft
         g.fillArc(320,553, 34, 34,180,90);//downleft
         g.fillArc(446,115, 34, 34,0,90);//upright
@@ -26,11 +24,22 @@ public class TrafficLights extends Canvas {
         //g.fillRect(372,607, 60, 80);
         g.fillRect(374,601, 56, 92);
         g.fillRect(367,605, 70, 80);
-
         g.fillArc(365, 600, 18, 18, 90,90);//upleft
         g.fillArc(365, 600+76, 18, 18, 180,90);//downleft
         g.fillArc(421, 600, 18, 18, 0,90);//upright
         g.fillArc(421, 600+76, 18, 18, 270,90);//downright
+
+        //Белый козырек
+        g.setColor(new Color(255,255,255));
+        g.fillArc(335,125,130,130,0,360);
+        g.fillArc(335,275,130,130,0,360);
+        g.fillArc(335,425,130,130,0,360);
+
+        g.setColor(new Color(50,50,50));
+        g.fillArc(335,142,130,130,0,360);
+        g.fillArc(335,292,130,130,0,360);
+        g.fillArc(335,442,130,130,0,360);
+
 
         //Круги
         g.setColor(new Color(255,0,1));
